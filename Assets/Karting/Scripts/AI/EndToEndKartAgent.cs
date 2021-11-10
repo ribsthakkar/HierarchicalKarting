513d3f0fda3f4358ee978d7e2db58d138cf3abc8
Aryaman Samyal <aryamansinghsamyal@gmail.com>
2021-11-10 13:58:24 -0600

    endtoendAgent_1

--- Assets/Karting/Scripts/AI/EndToEndKartAgent.cs
+++ Assets/Karting/Scripts/AI/EndToEndKartAgent.cs
@@ -11,21 +11,107 @@ using Unity.MLAgents.Policies;
 
 namespace KartGame.AI
 {
-
-    /// <summary>
-    /// The KartAgent will drive the inputs for the KartController.
-    /// </summary>
     public class EndToEndKartAgent : KartAgent
     {
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            episodeSteps += 1;
+        }
+
         protected override void Awake()
         {
             base.Awake();
+            var behaviorParameters = GetComponent<BehaviorParameters>();
+            var brainParameters = behaviorParameters.BrainParameters;
+            brainParameters.VectorObservationSize = Sensors.Length + sectionHorizon + 5 + 8 * otherAgents.Length;
+        }
+
+        protected override void setLaneDifferenceDivider(int sectionIndex, int lane)
+        {
+            LaneDifferenceRewardDivider = 1.0f;
         }
 
         public override void CollectObservations(VectorSensor sensor)
         {
+            sensor.AddObservation(m_Kart.LocalSpeed());
+            sensor.AddObservation(m_Acceleration);
+            sensor.AddObservation(m_Lane);
+            sensor.AddObservation(m_LaneChanges);
+            sensor.AddObservation(m_envController.Sections[m_SectionIndex % m_envController.Sections.Length].transform.parent.GetComponent<MeshCollider>().sharedMesh.name == "ModularTrackStraight");
+            sensor.AddObservation(m_Kart.TireWearProportion());
+            foreach (KartAgent agent in otherAgents)
+            {
+                sensor.AddObservation(agent.m_Kart.LocalSpeed());
+                sensor.AddObservation(agent.m_Acceleration);
+                sensor.AddObservation(agent.m_Lane);
+                // sensor.AddObservation(agent.m_LaneChanges);
+                sensor.AddObservation(agent.gameObject.activeSelf);
+                sensor.AddObservation(m_envController.Sections[agent.m_SectionIndex % m_envController.Sections.Length].transform.parent.GetComponent<MeshCollider>().sharedMesh.name == "ModularTrackStraight");
+                sensor.AddObservation((agent.m_Kart.transform.position - m_Kart.transform.position).magnitude);
+                sensor.AddObservation(Vector3.SignedAngle(m_Kart.transform.forward, agent.m_Kart.transform.position, Vector3.up));
+
+            }
+
+            // Add an observation for direction of the agent to the next checkpoint and lane.
+            for (int i = m_SectionIndex + 1; i < m_SectionIndex + 1 + sectionHorizon; i++)
+            {
+                var next = (i) % m_envController.Sections.Length;
+                var nextSection = m_envController.Sections[next];
+                if (nextSection == null)
+                    return;
+                if (m_UpcomingLanes.ContainsKey(next))
+                {
+                    BoxCollider targetLaneInSection = nextSection.getBoxColliderForLane(m_UpcomingLanes[next]);
+                    var direction = (targetLaneInSection.transform.position - m_Kart.transform.position).normalized;
+                    sensor.AddObservation(Vector3.Dot(m_Kart.Rigidbody.velocity.normalized, direction));
+
+                    if (ShowRaycasts)
+                        Debug.DrawLine(AgentSensorTransform.position, targetLaneInSection.transform.position, Color.magenta);
+                }
+                else
+                {
+                    sensor.AddObservation(3.5f);
+                }
+            }
+            for (var i = 0; i < Sensors.Length; i++)
+            {
+                var current = Sensors[i];
+                var xform = current.Transform;
+                var hitTrack = Physics.Raycast(AgentSensorTransform.position, xform.forward, out var hitTrackInfo,
+                    current.RayDistance, TrackMask, QueryTriggerInteraction.Ignore);
+
+                var hitAgent = Physics.Raycast(AgentSensorTransform.position, xform.forward, out var hitAgentInfo,
+                    current.RayDistance, AgentMask, QueryTriggerInteraction.Ignore);
+
+                if (hitTrack)
+                {
+                    if (hitTrackInfo.distance < current.WallHitValidationDistance && (!hitAgent || hitTrackInfo.distance < hitAgentInfo.distance))
+                    {
+                        m_HitOccured = true;
+                        m_envController.ResolveEvent(Event.HitWall, this, null);
+                    }
+                    sensor.AddObservation(hitTrackInfo.distance);
+                }
+                else if (hitAgent)
+                {
+                    if (hitAgentInfo.distance < current.AgentHitValidationDistance)
+                    {
+                        m_HitOccured = true;
+                        m_LastAccumulatedReward += m_envController.OpponentHitPenalty;
+                        hitAgents.Add(m_envController.AgentBodies[hitAgentInfo.collider.attachedRigidbody]);
+                        m_envController.ResolveEvent(Event.HitOpponent, this, hitAgents);
+                    }
+                    sensor.AddObservation(hitAgentInfo.distance);
+                }
+                else
+                {
+                    sensor.AddObservation(current.RayDistance);
+                }
+            }
         }
 
     }
+
 
 }
